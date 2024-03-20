3d1ee4b91585cc0ab41c5e1c408aeebf627c08a9
edsonwade <vanilsonmuhongo@gamil.com>
2024-03-20 23:31:19 +0000

    done implementing departments service.

---
+++ backend/department-service/src/main/java/code/with/vanilson/department/DepartmentDto.java
@@ -0,0 +1,23 @@
+package code.with.vanilson.department;
+
+import lombok.Data;
+
+@Data
+public class DepartmentDto {
+    private long departmentId;
+    private String departmentName;
+    private String departmentDescription;
+    private String departmentCode;
+
+    public DepartmentDto() {
+        //default constructor
+    }
+
+    public DepartmentDto(long departmentId, String departmentName, String departmentDescription,
+                         String departmentCode) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.departmentDescription = departmentDescription;
+        this.departmentCode = departmentCode;
+    }
+}
