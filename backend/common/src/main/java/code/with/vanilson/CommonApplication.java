c09a7d1035bf2743b59b9e761fe4fca7f949529f
edsonwade <vanilsonmuhongo@gamil.com>
2024-03-21 20:26:02 +0000

    defined swagger path http://localhost:8082/api-docs

---
+++ backend/common/src/main/java/code/with/vanilson/CommonApplication.java
@@ -0,0 +1,12 @@
+package code.with.vanilson;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class CommonApplication {
+    public static void main(String[] args) {
+        run(CommonApplication.class, args);
+    }
+}
